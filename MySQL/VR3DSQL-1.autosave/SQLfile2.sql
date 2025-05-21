8364e8be3c134b62cb78ba2bb05f5f847013b445
ViniciusRodrigues0079 <vinicius.rgmp.escolar@gmail.com>
2025-05-21 14:04:32 -0300

    Concluído atividade sobre banco de dados

--- MySQL/VR3DSQL-1.autosave/SQLfile2.sql
+++ MySQL/VR3DSQL-1.autosave/SQLfile2.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE Powers;
 USE Powers;
 CREATE TABLE Top_10_Powers (
-    'id' INT NOT NULL AUTO_INCREMENT,
-    'power' VARCHAR (255) NOT NULL,
-    'uses' VARCHAR (255) NOT NULL,
-    'rank' INT NOT NULL,
-    PRIMARY KEY  ('id');
+    id INT NOT NULL AUTO_INCREMENT,
+    power VARCHAR (255) NOT NULL,
+    uses VARCHAR (255) NOT NULL,
+    ranks INT NOT NULL,
+    PRIMARY KEY  (id)
 );
 CREATE TABLE Daily_utilities (
-    'ut_id' INT NOT NULL AUTO_INCREMENT,
-    'daily_utility' VARCHAR (255) NOT NULL,
-    PRIMARY KEY ('ut_id'),
-    CONSTRAINT pow_ut FOREIGN KEY ('ut_id') REFERENCES 'Top_10_Powers' ('id') ON DELETE CASCADE ON UPDATE RESTRICT
+    ut_id INT NOT NULL AUTO_INCREMENT,
+    daily_utility VARCHAR (255) NOT NULL,
+    PRIMARY KEY (ut_id),
+    CONSTRAINT pow_ut FOREIGN KEY (ut_id) REFERENCES Top_10_Powers (id) ON DELETE CASCADE ON UPDATE RESTRICT
 );
-INSERT INTO Top_10_Powers (power, uses, rank) VALUES
+INSERT INTO Top_10_Powers (power, uses, ranks) VALUES
 ('Teleport', 'The ability to teleport yourself anywhere.', 4),
 ('Fly', 'The ability to fly in the air.', 8),
 ('Ultra Speed', 'The ability to run, react and live in ultra high speed.', 7),
@@ -38,14 +38,14 @@ INSERT INTO Daily_utilities (daily_utility) VALUES
 ('It can be used to become an human shield.');
 
 SELECT * FROM Top_10_Powers
-ORDER BY rank ASC;
+ORDER BY ranks ASC;
 
 SELECT * FROM Top_10_Powers
-ORDER BY rank DESC LIMIT 5;
+ORDER BY ranks DESC LIMIT 5;
 
 SELECT * FROM  Top_10_Powers
 WHERE id NOT IN (5, 8, 9)
-ORDER BY rank ASC;
+ORDER BY ranks ASC;
 
 SELECT power AS 'Power Name' FROM Top_10_Powers
 WHERE power LIKE 'Control'
@@ -53,10 +53,4 @@ ORDER BY power ASC;
 
 SELECT * FROM Top_10_Powers
 WHERE (id < 3 OR uses NOT LIKE 'manipulate')
-ORDER BY rank ASC;
-
-SELECT * FROM Daily_utilities
-ORDER BY ut_id
-WHERE (id < 4);
-
-/*Still need 4 more SELECT from Daily_utilities
+ORDER BY ranks ASC;
